dceea8b0b163c76070b57941205051bc4fd8bb31
Charles Cao <caotaotao@gmail.com>
2023-07-11 17:57:10 +0800

    upload hello world

--- hello_world/app/main.py
+++ hello_world/app/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
-
+import sys
+from web3 import Web3, HTTPProvider
 from datetime import datetime
-
-now = datetime.now()  # current date and time
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World! %s" % now.strftime("%m/%d/%Y, %H:%M:%S")}
+    chain_name = "filecoin_testnet"
+    try:
+        w3 = Web3(HTTPProvider('https://rpc.ankr.com/' + chain_name))
+    except:
+        print("[!] WARNING: INVALID HTTPProvider ('web3' env variable)")
+        sys.exit("NOPE")
+
+    wallet = '0x96216849c49358B10257cb55b28eA603c874b05E'
+
+    wallet_balance = w3.eth.get_balance(wallet)
+    return {"data":
+        {
+            "chain_name": chain_name,
+            "provider": "Fogmeta",
+            "wallet": wallet,
+            "balance": wallet_balance
+        }}
